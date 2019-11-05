8a9a12357df9a6ce820fc0b70492756c95b8f74b
Clay Dunston <claydunston@gmail.com>
2019-11-05 15:11:20 -0600

    More models

---
+++ lib/stubify/build/graphql/models.rb
@@ -0,0 +1,49 @@
+module Stubify
+  module Build
+    module GraphQL
+
+      # Name Description pair
+      class ND < Struct.new(:name, :description)
+        # @!attribute [rw] name
+        #   @return [String]
+
+        # @!attribute [rw] description
+        #   @return [String]
+      end
+
+      # Enum Object
+      class Enum < Struct.new(:name, :description, :values)
+        # @!attribute [rw] name
+        #   @return [String]
+
+        # @!attribute [rw] description
+        #   @return [String]
+
+        # @!attribute [rw] values
+        #   @return [Array<Hash>]
+      end
+
+      class Argument < Struct.new(:name, :description, :default, :required, :type)
+        # @!attribute [rw] name
+        #   @return [String]
+
+        # @!attribute [rw] description
+        #   @return [String]
+
+        # @!attribute [rw] default
+        #   @return [String]
+
+        # @!attribute [rw] required
+        #   @return [Boolean]
+
+        # @!attribute [rw] type
+        #   @return [String]
+      end
+
+      class Field < Struct.new(:name, :description, :args, :required, :type, :list)
+
+      end
+
+    end
+  end
+end
---
+++ lib/stubify/models/product/product.rb
@@ -0,0 +1,8 @@
+module Stubify
+  # The Product resource lets you update and create products in a merchant's store.
+  # See the [API documentation](https://help.shopify.com/en/api/reference/orders/order).
+  class Product
+
+  end
+
+end
