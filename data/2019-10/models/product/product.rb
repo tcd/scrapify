14e9bfc9953f1f580f86d15d75e79068c228d012
Clay Dunston <claydunston@gmail.com>
2019-12-01 10:40:52 -0600

    Remove generated code

--- data/2019-10/models/product/product.rb
+++
@@ -1,8 +0,0 @@
-module Scrapify
-  # The Product resource lets you update and create products in a merchant's store.
-  # See the [API documentation](https://help.shopify.com/en/api/reference/orders/order).
-  class Product
-
-  end
-
-end
